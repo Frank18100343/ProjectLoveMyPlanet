590d0f4326f287d964aa2d7dec038ec11204aba4
Frank Fernando Bruno Perez Aliaga <fpereza@valtx.pe>
2021-10-30 19:38:29 -0500

    Se agregó Layaout Voluntario y Auspiciador

---
+++ app/src/main/java/dev/frankperez/projectlovemyplanet/RegVoluntarioActivity.kt
@@ -0,0 +1,11 @@
+package dev.frankperez.projectlovemyplanet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegVoluntarioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reg_voluntario)
+    }
+}
