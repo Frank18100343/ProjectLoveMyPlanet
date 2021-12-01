c62773e5de35dcd29ae2e6cba09a10699a457791
18200275 <82484239+18200275@users.noreply.github.com>
2021-12-01 13:30:26 -0500

    Creacion Layout Registro y Listado de Actividades

---
+++ app/src/main/java/dev/frankperez/projectlovemyplanet/ui/fragments/HelperFecha.java
@@ -0,0 +1,20 @@
+package dev.frankperez.projectlovemyplanet.ui.fragments;
+
+import android.app.Activity;
+import android.app.Application;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class HelperFecha extends Application {
+
+}
